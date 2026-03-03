0f9e6dab4d2138c926b215ede08ef53a1fc93f62
Satendra017 <yksatendra987@gmail.com>
2026-03-04 00:40:59 +0530

    auto create tables on startup

--- app/main.py
+++ app/main.py
@@ -1,29 +1,28 @@
 from fastapi import FastAPI
-# from app.db.session import engine
-# from app.db.base import Base
-# from app.models.user import User
-# from app.models.chat_session import ChatSession
-# from app.models.chat_message import ChatMessage
+from app.db.session import engine
+from app.db.base import Base
+from app.models.user import User
+from app.models.chat_session import ChatSession
+from app.models.chat_message import ChatMessage
+from app.models.usage_log import UsageLog
 from app.api.auth_routes import router as auth_router
 from app.api.chat_routes import router as chat_router
 from app.api.session_routes import router as session_router
 from fastapi.middleware.cors import CORSMiddleware
-# from contextlib import asynccontextmanager
-# import app.db.init_db
+from contextlib import asynccontextmanager
 
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     async with engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
-#     yield
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
 
-# app = FastAPI(title="Enterprise GenAI Platform", lifespan=lifespan)
-app = FastAPI(title="Enterprise GenAI Platform")
+app = FastAPI(title="Enterprise GenAI Platform", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Later replace with Streamlit URL
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
